3954df0c98c872eee7cf7a3dbca7d2a5f76ae43b
myrosvas <myrosvas@gmail.com>
2018-12-24 23:22:58 +0200

    fetch done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BubbleComponent } from './bubble/bubble.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HomeComponent } from './home/home.component';
+import { GetPredictionComponent } from './get-prediction/get-prediction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BubbleComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    LogoutComponent,
+    HomeComponent,
+    GetPredictionComponent
   ],
   imports: [
     BrowserModule,
